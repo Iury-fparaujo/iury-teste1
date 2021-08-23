974e91d73a8e517fe61fa924c4ba830f954a6ba2
Iury-fparaujo <iury.araujo@corebiz.ag>
2021-08-23 17:44:17 -0300

    23-08 Estilização

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,10 +2,10 @@ import './index.scss'
 
 export const home = () => {
     let planos = [
-        {id: 177, name: "Campeonato Mega Horse 2019", img: "https://clickqi.vteximg.com.br/arquivos/ids/156778-130-130/FP_MEgaHorse2019-calendario.jpg?v=636746724307200000", price: 0.0},
-        {id: 196 , name: "Campeonato de Team Penning - Haras Pati 2018", img: "https://clickqi.vteximg.com.br/arquivos/ids/156767-130-130/campeonato-haras-pati-2018.jpg?v=636742523750530000", price: 0.0}, 
-        {id: 209, name: "Prova de Patrocínio - Team Penning. 2019", img: "https://clickqi.vteximg.com.br/arquivos/ids/156873-130-130/FP_ProvaPatrocinioMG1.jpg?v=636849600675170000", price: 0.0},
-        {id: 175, name: "6° Prova de Team Penning  - Nossa Senhora Aparecida", img: "https://clickqi.vteximg.com.br/arquivos/ids/156769-130-130/6-prova-do-aureli-2018.jpg?v=636742540247170000", price: 0.0},
+        {id: 177, name: "Campeonato Mega Horse 2019", img: "https://clickqi.vteximg.com.br/arquivos/ids/156778-130-130/FP_MEgaHorse2019-calendario.jpg?v=636746724307200000", price: 10.0},
+        {id: 196 , name: "Campeonato de Team Penning - Haras Pati 2018", img: "https://clickqi.vteximg.com.br/arquivos/ids/156767-130-130/campeonato-haras-pati-2018.jpg?v=636742523750530000", price: 20.0}, 
+        {id: 209, name: "Prova de Patrocínio - Team Penning. 2019", img: "https://clickqi.vteximg.com.br/arquivos/ids/156873-130-130/FP_ProvaPatrocinioMG1.jpg?v=636849600675170000", price: 30.0},
+        {id: 175, name: "6° Prova de Team Penning  - Nossa Senhora Aparecida", img: "https://clickqi.vteximg.com.br/arquivos/ids/156769-130-130/6-prova-do-aureli-2018.jpg?v=636742540247170000", price: 40.0},
     ]
 
     let carrinho = []
@@ -24,7 +24,7 @@ export const home = () => {
                 $('.cart-middle').find('ul').html('')
                 $.map(carrinho, (el) => {
                     document.querySelector('.cart-middle ul').innerHTML += 
-                    `<li><img src=${el.img}/> <div class="informacoes-produto"><p>${el.name}</p></div></li>`
+                    `<li><img src=${el.img}/> <div class="informacoes-produto"><p>${el.name}</p><span>R$: ${el.price},00</span></div></li>`
                 })
                 console.log(carrinho)
             }
@@ -39,7 +39,8 @@ export const home = () => {
                     let objeto = {
                         id: el.id,
                         img: el.img,
-                        name: el.name
+                        name: el.name,
+                        price: el.price
                     }
                     carrinho.push(objeto)
                     atualizaCarrinho()  
